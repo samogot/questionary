c5a0d3ef6f37fed03ebf5eea14bab16ef84957ac
Mykhaylo Naydonov <iris_psy@ukr.net>
2025-12-21 13:55:06 +0100

    update windows bat

--- transform.bat
+++ transform.bat
@@ -1,2 +1,2 @@
-venv\Scripts\activate.bat
-python3 transform.py --input-dir=in --output-dir=out --remove-input --block-on-error --no-bulletin-all-combinations
+call venv\Scripts\activate.bat
+python transform.py --input-dir=in --output-dir=out --remove-input --block-on-error --no-bulletin-all-combinations
